d2b41e1df96f92fe365c3ea9ead72abfbc22a76f
EmaSciacca <ema.sciaccaluga@gmail.com>
2021-07-30 12:58:29 +0200

    Added grid with first logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // Header string
-  headerStr = "X Turn";
+  headerStr: string;
+  // turn number (1-9)
+  private turnNum: number;
+  // current player
+  private currPlayer: string;
+  // grid view
+  gridView: string[][];
+
+  constructor() {
+    this.currPlayer = "X";
+    this.headerStr = this.currPlayer + " Turn";
+    this.turnNum = 1;
+    this.gridView = [[], [], []];
+    this.initializeGridView();
+  }
+
+  playTurn(rowIndex: number, colIndex: number) {
+    console.log("Player " + this.currPlayer + " played at (" + rowIndex + ", " + colIndex + ")");
+  }
 
   restartGame() {
     console.log("restartGame invoked");
+  }
+
+  private initializeGridView() {
+    this.gridView = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ]
   }
 }
